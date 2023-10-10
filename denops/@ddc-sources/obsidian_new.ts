9c01591486c8c6b593d24b3ecc2f5f051fbce6c1
keimoriyama <keischwiiz@gmail.com>
2023-10-10 21:41:34 +0900

    add obsidian new

--- denops/@ddc-sources/obsidian_new.ts
+++ denops/@ddc-sources/obsidian_new.ts
@@ -1,55 +1,92 @@
 import {
   BaseSource,
   Context,
+  DdcGatherItems,
   DdcOptions,
   DdcUserData,
   Item,
   OnCallback,
   SourceOptions,
-  UserSource,
+  UserData,
 } from "https://deno.land/x/ddc_vim@v3.4.0/types.ts";
 import { Denops } from "https://deno.land/x/ddc_vim@v3.4.0/deps.ts";
 import { expand, globals } from "./deps.ts";
 import { walk } from "https://deno.land/std@0.92.0/fs/mod.ts";
-import { readfile } from "https://deno.land/x/denops_std@v5.0.1/function/mod.ts";
+import { type OnCompleteDoneArguments } from "https://deno.land/x/ddc_vim@v4.0.5/base/source.ts";
+import { writefile } from "https://deno.land/x/denops_std@v5.0.1/function/mod.ts";
 type Params = Record<never, never>;
 
+type ObsidianNotes = {
+  id: string;
+  filename: string;
+  noteDir: string;
+};
+
 export class Source extends BaseSource<Params> {
-  override async onCompleteDone(
-    denops: Denops,
-    context: Context,
-    onCallback: OnCallback,
-    options: DdcOptions,
-    userSource: UserSource,
-    userData: DdcUserData,
-  ): Promise<void> {
-    // 存在しないファイル名を補完した場合、そのファイルを作成する
-  }
   override async gather(args: {
     denops: Denops;
     options: DdcOptions;
     sourceOptions: SourceOptions;
     sourceParams: Params;
     completeStr: string;
-  }): Promise<Item[]> {
-    let items: Item[] = [];
+  }): Promise<DdcGatherItems<ObsidianNotes>> {
+    const items: Item<ObsidianNotes>[] = [];
+    const target = args.context.input;
     const base_dir: string = await expand(
       args.denops,
       await globals.get(args.denops, "base_dir"),
     );
+    console.log("hello");
     const file_in_vault = await get_file_in_vault(base_dir);
     let filename = await gen_filename();
     while (!check(file_in_vault, filename)) {
       filename = await gen_filename();
     }
-    // カーソルの下で[[]]に囲まれているの文字列を取得
-    console.log(args);
-    items.push({ word: filename });
-    return items;
+
+    items.push({
+      word: target,
+      user_data: { id: target, filename: filename, noteDir: base_dir },
+    });
+    return { items: items, isIncomplete: true };
   }
   override params(): Params {
     return {};
   }
+  override async onCompleteDone(
+    {
+      denops,
+      userData,
+      context,
+      sourceParams,
+    }: OnCompleteDoneArguments<Params, UserData>,
+  ): Promise<void> {
+    console.log(context);
+
+    const content = await note_template(userData.id, userData.filename);
+    await writefile(
+      denops,
+      content,
+      userData.noteDir + "/" + userData.filename + ".md",
+    );
+    const replace_str = userData.filename + "|" + userData.id;
+    return;
+  }
+}
+
+async function note_template(
+  alias: string,
+  filename: string,
+): Promise<string[]> {
+  const id = filename.replace(".md", "");
+  return [
+    "---",
+    `id: \"${id}\"`,
+    "aliases:",
+    `- \"${alias}\"`,
+    "tags:",
+    '- ""',
+    "--- ",
+  ];
 }
 
 async function check(
@@ -74,7 +111,6 @@ async function gen_filename(): Promise<string> {
     }
   }
   filename += "_" + String(Math.floor(Math.random() * 10000));
-  console.log(filename);
   return filename;
 }
 

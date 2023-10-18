1fa8c972b7a0fe21bd0d27ba9c7a652c6b5441fa
keimoriyama <keischwiiz@gmail.com>
2023-10-18 22:33:57 +0900

    fix completion

--- denops/@ddc-sources/obsidian.ts
+++ denops/@ddc-sources/obsidian.ts
@@ -1,19 +1,28 @@
 import {
   BaseSource,
-  Context,
   DdcOptions,
-  DdcUserData,
   Item,
-  OnCallback,
   SourceOptions,
-  UserSource,
 } from "https://deno.land/x/ddc_vim@v3.4.0/types.ts";
 import { Denops } from "https://deno.land/x/ddc_vim@v3.4.0/deps.ts";
-import { expand, globals } from "./deps.ts";
 import { walk } from "https://deno.land/std@0.92.0/fs/mod.ts";
 import { readfile } from "https://deno.land/x/denops_std@v5.0.1/function/mod.ts";
 import { get_base_dir } from "../obsidian/utils.ts";
+
+import {
+  getbufline,
+  winbufnr,
+  writefile,
+} from "https://deno.land/x/denops_std@v5.0.1/function/mod.ts";
+
+import { type OnCompleteDoneArguments } from "https://deno.land/x/ddc_vim@v4.0.5/base/source.ts";
+
 type Params = Record<never, never>;
+type ObsidianNotes = {
+  id: string;
+  filename: string;
+  noteDir: string;
+};
 
 export class Source extends BaseSource<Params> {
   override async gather(args: {
@@ -23,8 +32,33 @@ export class Source extends BaseSource<Params> {
     sourceParams: Params;
     completeStr: string;
   }): Promise<Item[]> {
-    let items: Item[] = [];
+    let items: Item[] = this.getCompletionFiles(args.denops);
+    if (items.length == 0) {
+      items = this.createNewNote(args);
+    }
+    return items;
+  }
+  override params(): Params {
+    return {};
+  }
+  async createNewNote(args: any): Promise<ObsidianNotes> {
+    const items: Item<ObsidianNotes>[] = [];
+    const target: string = args.context.input;
     const base_dir: string = await get_base_dir(args.denops);
+    const file_in_vault = await get_file_in_vault(base_dir);
+    let filename = await gen_filename();
+    while (!check(file_in_vault, filename)) {
+      filename = await gen_filename();
+    }
+    items.push({
+      word: target,
+      user_data: { id: target, filename: filename, noteDir: base_dir },
+    });
+    return { items: items, isIncomplete: true };
+  }
+  async getCompletionFiles(denops: Denops): Promise<Item[]> {
+    let items: Item[] = [];
+    const base_dir: string = await get_base_dir(denops);
     // あるNoteのファイル名とタグを取得する
     for await (
       const entry of walk(base_dir, {
@@ -35,7 +69,7 @@ export class Source extends BaseSource<Params> {
     ) {
       const path = entry.path.split("/");
       const filename = path[path.length - 1].replace(".md", "");
-      const file_content = await readfile(args.denops, entry.path);
+      const file_content = await readfile(denops, entry.path);
       const alias_idx = file_content.indexOf("aliases:");
       const tags_idx = file_content.indexOf("tags:");
       const regex = /"(.*)"/g;
@@ -52,7 +86,92 @@ export class Source extends BaseSource<Params> {
     }
     return items;
   }
-  override params(): Params {
-    return {};
+  override async onCompleteDone(
+    {
+      denops,
+      userData,
+      context,
+      sourceParams,
+    }: OnCompleteDoneArguments<Params, UserData>,
+  ): Promise<void> {
+    const content = await note_template(userData.id, userData.filename);
+    await writefile(
+      denops,
+      content,
+      userData.noteDir + "/" + userData.filename + ".md",
+    );
+    const replace_str = userData.filename + "|" + userData.id;
+    // [[]]で囲まれている文字列を変換する
+    const line = context.lineNr;
+    const bufnr = await winbufnr(denops, 0);
+    const line_under_cursor = await getbufline(denops, bufnr, line);
+    const target = line_under_cursor[0];
+    let replacedString = "[[" + target.replace(
+      /\[\[(.*?)\]\]/g,
+      replace_str,
+    ).replace("[[", "");
+    if (replacedString.indexOf("]]") == -1) {
+      replacedString += "]]";
+    }
+    console.log(replacedString);
+    await denops.call("setline", line, replacedString);
+    return;
   }
+}
+function note_template(
+  alias: string,
+  filename: string,
+): string[] {
+  const id = filename.replace(".md", "");
+  alias = alias.replace("\[\[", "").replace("\]\]", "");
+  return [
+    "---",
+    `id: \"${id}\"`,
+    "aliases:",
+    `- \"${alias}\"`,
+    "tags:",
+    '- ""',
+    "--- ",
+  ];
+}
+
+async function check(
+  files_in_vault: string[],
+  filename: string,
+): Promise<boolean> {
+  for (const file in files_in_vault) {
+    if (file.includes(filename)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+async function gen_filename(): Promise<string> {
+  const alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+  let filename = "";
+  while (filename.length < 4) {
+    const n = Math.floor(Math.random() * 100);
+    if (n < 26) {
+      filename += alpha[n];
+    }
+  }
+  filename += "_" + String(Math.floor(Math.random() * 10000));
+  return filename;
+}
+
+async function get_file_in_vault(
+  base_dir: string,
+): Promise<string[]> {
+  let files = [];
+  for await (
+    const entry of walk(base_dir, {
+      includeDirs: false,
+      includeFiles: true,
+      exts: [".md"],
+    })
+  ) {
+    files.push(entry.path);
+  }
+  return files;
 }
